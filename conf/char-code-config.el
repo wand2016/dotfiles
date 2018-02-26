abfa639acb58fe2775bdcebaf8ef212657b8765b
堀山大樹 <horiyama_daiki@MacBook-Pro.local>
2018-02-26 12:21:41 +0900

    [modify] char-set settings updated

--- conf/char-code-config.el
+++ conf/char-code-config.el
@@ -5,14 +5,20 @@
 ;; 文字コードの設定
 (set-language-environment "Japanese")
 (prefer-coding-system 'utf-8)
-;; Windows用
-(set-file-name-coding-system 'cp932)
 
-;; シェルの文字コードをcmdに合わせる
-(add-hook 'shell-mode-hook
-          (lambda ()
-            (set-buffer-process-coding-system 'cp932 'cp932)
-            ))
+;; windowsにて、shellの文字化けを回避
+(when (eq system-type 'windows-nt)
+  ;; Windows用
+  (set-file-name-coding-system 'cp932)
+  (add-hook 'shell-mode-hook
+            (lambda ()
+              (set-buffer-process-coding-system 'sjis 'sjis))))
+
+;; mac
+(when (eq system-type 'darwin)
+  (add-hook 'shell-mode-hook
+            (lambda ()
+              (set-buffer-process-coding-system 'utf-8-hfs 'utf-8-hfs))))
 
 
 (provide 'char-code-config)
