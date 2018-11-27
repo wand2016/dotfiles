97343c1300b80bc48b3b11b888cd8c8de04291dd
wand <d19921207@gmail.com>
2018-11-28 08:34:26 +0900

    tweak company-tern conf
    install yasnippet

--- conf/js-config.el
+++ conf/js-config.el
@@ -5,8 +5,6 @@
 
 ;; 拡張子jsのファイルを開いたときjs2-modeに
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-;; (setq comment-style 'multi-line)
-
 
 ;; インデントの設定
 (defun js-indent-hook ()
@@ -41,75 +39,44 @@
 
 ;; スタイルチェック
 ;; 重いのでoff
-;(add-hook 'js2-mode-hook 'flymake-mode)
+                                        ;(add-hook 'js2-mode-hook 'flymake-mode)
 
-
-;; コメントの設定
-(defun js-comment-hook ()
-  ;; 新たなcomment-box
-  (defun my-comment-box (beg end &optional arg)
-	(interactive "*r\np")
-	;; commentの設定を退避
-	(setq-local comment-start-tmp comment-start)
-	(setq-local comment-end-tmp comment-end)
-	;; /**/形式でボックスを作る
-	(setq-local comment-start "/*")
-	(setq-local comment-end "*/")
-	(comment-box beg end arg)
-	;; 設定を書き戻す
-	(setq-local comment-start comment-start-tmp)
-	(setq-local comment-end comment-end-tmp))
-  )
-
-;; js2-modeで上記コメント設定
-(add-hook 'js2-mode-hook 'js-comment-hook)
 
 ;; ========================================
 ;;  補完の設定
+;;  company-tern
 ;; ========================================
 
-;; 自身をマークする いらねえ
-(setq company-tern-property-marker "")
-
-;; depth絡みの不具合があるらしい
-;; 対策
+;; depth絡みの不具合への対策
+;; override
 (defun company-tern-depth (candidate)
   "Return depth attribute for CANDIDATE. 'nil' entries are treated as 0."
   (let ((depth (get-text-property 0 'depth candidate)))
     (if (eq depth nil) 0 depth)))
-(add-hook 'js2-mode-hook 'tern-mode)
 
-;; backendに追加
+(defun js2-company-tern-hook ()
+  (when (locate-library "tern")
+    ;; .term-port を作らない
+    (setq tern-command '("tern" "--no-port-file"))
+    (tern-mode t)))
+
+(add-hook 'js2-mode-hook 'js2-company-tern-hook)
+
+;; backend追加
+(add-to-list 'company-backends 'company-tern)
 ;; company-dabbrev-codeは現在開いているバッファからワードを拾ってくる
-(add-to-list 'company-backends '(company-tern :with company-dabbrev-code))
-;(add-to-list 'company-backends 'company-tern)
+;; (add-to-list 'company-backends '(company-tern :with company-dabbrev-code))
 
-
-
-;; よく使うやつ
-(add-hook 'js2-mode-hook
-          (lambda ()
-            ;; よく使うものをキーバインド登録
-            (define-key js2-mode-map (kbd "s-f") "function () {}")
-            (define-key js2-mode-map (kbd "s-l") "console.log();")
-            (define-key js2-mode-map (kbd "s-q") "''")
-            (define-key js2-mode-map (kbd "s-Q") "\"\"")
-
-            ;; .term-port を作らない            
-            (when (locate-library "tern")
-              (setq tern-command '("tern" "--no-port-file"))
-              (tern-mode t)
-              (eval-after-load 'tern
-                '(progn
-                   (require 'tern-auto-complete)
-                   (tern-ac-setup))))))
-
-
+;; ========================================
 ;; js2-modeでjsdocを使う
 ;; C-ciにバインド
-(add-hook 'js2-mode-hook
-          (lambda ()
-            (local-set-key "\C-ci" 'js-doc-insert-function-doc)
-            (local-set-key "@" 'js-doc-insert-tag)))
+;; ========================================
+
+(defun js2-jsdoc-hook ()
+  (local-set-key "\C-ci" 'js-doc-insert-function-doc)
+  (local-set-key "@" 'js-doc-insert-tag))
+
+(add-hook 'js2-mode-hook 'js2-jsdoc-hook)
+
 
 (provide 'js-config)
