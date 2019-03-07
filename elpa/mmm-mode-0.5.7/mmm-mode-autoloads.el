5b61ac2b9c120a2057011cdeaa793ad2bd3aeb45
Horiyama Daiki <d19921207@gmail.com>
2019-03-07 12:41:16 +0900

    merge

---
+++ elpa/mmm-mode-0.5.7/mmm-mode-autoloads.el
@@ -0,0 +1,54 @@
+;;; mmm-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "mmm-erb" "../../../../.emacs.d/elpa/mmm-mode-0.5.7/mmm-erb.el"
+;;;;;;  "0764ba6d1ff015256664e7f8e4b188c5")
+;;; Generated autoloads from ../../../../.emacs.d/elpa/mmm-mode-0.5.7/mmm-erb.el
+
+(autoload 'html-erb-mode "mmm-erb" "\
+
+
+\(fn)" t nil)
+
+(autoload 'nxml-web-mode "mmm-erb" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "mmm-vars" "../../../../.emacs.d/elpa/mmm-mode-0.5.7/mmm-vars.el"
+;;;;;;  "b2368ef69b06d22ee791cf159eb8c2e7")
+;;; Generated autoloads from ../../../../.emacs.d/elpa/mmm-mode-0.5.7/mmm-vars.el
+
+(autoload 'mmm-add-classes "mmm-vars" "\
+Add the submode classes CLASSES to `mmm-classes-alist'.
+
+\(fn CLASSES)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/mmm-mode-0.5.7/mmm-auto.el"
+;;;;;;  "../../../../.emacs.d/elpa/mmm-mode-0.5.7/mmm-class.el" "../../../../.emacs.d/elpa/mmm-mode-0.5.7/mmm-cmds.el"
+;;;;;;  "../../../../.emacs.d/elpa/mmm-mode-0.5.7/mmm-compat.el"
+;;;;;;  "../../../../.emacs.d/elpa/mmm-mode-0.5.7/mmm-cweb.el" "../../../../.emacs.d/elpa/mmm-mode-0.5.7/mmm-defaults.el"
+;;;;;;  "../../../../.emacs.d/elpa/mmm-mode-0.5.7/mmm-erb.el" "../../../../.emacs.d/elpa/mmm-mode-0.5.7/mmm-mason.el"
+;;;;;;  "../../../../.emacs.d/elpa/mmm-mode-0.5.7/mmm-mode-autoloads.el"
+;;;;;;  "../../../../.emacs.d/elpa/mmm-mode-0.5.7/mmm-mode-pkg.el"
+;;;;;;  "../../../../.emacs.d/elpa/mmm-mode-0.5.7/mmm-mode.el" "../../../../.emacs.d/elpa/mmm-mode-0.5.7/mmm-myghty.el"
+;;;;;;  "../../../../.emacs.d/elpa/mmm-mode-0.5.7/mmm-noweb.el" "../../../../.emacs.d/elpa/mmm-mode-0.5.7/mmm-region.el"
+;;;;;;  "../../../../.emacs.d/elpa/mmm-mode-0.5.7/mmm-rpm.el" "../../../../.emacs.d/elpa/mmm-mode-0.5.7/mmm-sample.el"
+;;;;;;  "../../../../.emacs.d/elpa/mmm-mode-0.5.7/mmm-univ.el" "../../../../.emacs.d/elpa/mmm-mode-0.5.7/mmm-utils.el"
+;;;;;;  "../../../../.emacs.d/elpa/mmm-mode-0.5.7/mmm-vars.el") (23680
+;;;;;;  29206 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; mmm-mode-autoloads.el ends here
