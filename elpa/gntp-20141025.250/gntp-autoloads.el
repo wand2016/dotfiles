0874036984c439e5d9a6bb51e219ff579481a03a
Horiyama Daiki <d19921207@gmail.com>
2019-03-13 19:01:09 +0900

    org-pomodoroの導入

---
+++ elpa/gntp-20141025.250/gntp-autoloads.el
@@ -0,0 +1,29 @@
+;;; gntp-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "gntp" "../../../../.emacs.d/elpa/gntp-20141025.250/gntp.el"
+;;;;;;  "7b58a129198ff997a1d9a964e13ef62f")
+;;; Generated autoloads from ../../../../.emacs.d/elpa/gntp-20141025.250/gntp.el
+
+(autoload 'gntp-notify "gntp" "\
+Send notification NAME with TITLE, TEXT, PRIORITY and ICON to SERVER:PORT.
+PORT defaults to `gntp-server-port'
+
+\(fn NAME TITLE TEXT SERVER &optional PORT PRIORITY ICON)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/gntp-20141025.250/gntp-autoloads.el"
+;;;;;;  "../../../../.emacs.d/elpa/gntp-20141025.250/gntp.el") (23688
+;;;;;;  35696 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; gntp-autoloads.el ends here
---
+++ elpa/log4e-20170401.1304/log4e-autoloads.el
@@ -0,0 +1,36 @@
+;;; log4e-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "log4e" "../../../../.emacs.d/elpa/log4e-20170401.1304/log4e.el"
+;;;;;;  "c507f635d4870084648215bb885c2db2")
+;;; Generated autoloads from ../../../../.emacs.d/elpa/log4e-20170401.1304/log4e.el
+
+(autoload 'log4e-mode "log4e" "\
+Major mode for browsing a buffer made by log4e.
+
+\\<log4e-mode-map>
+\\{log4e-mode-map}
+
+\(fn)" t nil)
+
+(autoload 'log4e:insert-start-log-quickly "log4e" "\
+Insert logging statment for trace level log at start of current function/macro.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/log4e-20170401.1304/log4e-autoloads.el"
+;;;;;;  "../../../../.emacs.d/elpa/log4e-20170401.1304/log4e.el")
+;;;;;;  (23688 35695 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; log4e-autoloads.el ends here
