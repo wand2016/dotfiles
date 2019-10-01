df20b1e6fdfb0e9cdda6f35afe6c08a576b0ec76
Daiki Horiyama <d-horiyama@ef-4.co.jp>
2019-10-01 17:01:31 +0900

    use-package導入、orgによるGTDのための設定

--- init.el
+++ init.el
@@ -144,6 +144,80 @@
 ;;;========================================
 (require 'docker-tramp-compat)
 
+
+
+
+;; org
+
+(use-package org
+  :commands org-agenda
+  :mode (("\\.org\\'" . org-mode))
+  :init
+  (setq org-directory "~/org/")
+  (setq org-default-notes-file (concat org-directory "main.org"))
+  (setq org-agenda-files (list org-directory))
+  (defun my:org-goto-inbox ()
+    (interactive)
+    (find-file org-default-notes-file))
+  :config
+  (setq system-time-locale "C")  ;; to avoid Japanese in the time stamp
+  (setq org-hide-leading-stars t)
+  (setq org-startup-folded nil)
+  ;; org-capture and enrty
+  (setq org-todo-keywords
+        '((sequence "TASK(t)" "WAIT(w)" "|" "DONE(d)" "ABORT(a)" "SOMEDAY(s)")))
+  (setq org-tag-alist '(("PROJECT" . ?p) ("MEMO" . ?m) ("PETIT" . ?t)))
+  (setq org-capture-templates
+        '(("t" "Task" entry (file+headline org-default-notes-file "inbox")
+           "** TASK %?\n   CREATED: %U\n")
+          ("i" "Idea" entry (file+headline org-default-notes-file "idea")
+           "** %?\n   CREATED: %U\n")))
+  (setq org-refile-targets '((org-agenda-files :maxlevel . 1)))
+  ;; org-clock
+  (setq org-log-done 'time)  ;; add a time stamp to the task when done
+  (setq org-clock-clocked-in-display 'frame-title)
+  :bind
+  (("C-c a" . org-agenda)
+   ("C-c c" . org-capture)
+   ("C-c g" . org-clock-goto)
+   ("C-c i" . my:org-goto-inbox)
+   :map org-mode-map
+   ("C-m" . org-return-indent)
+   ("M-n" . org-forward-same-level)
+   ("M-p" . org-backward-same-level)
+   ("C-c h" . helm-org-in-buffer-headings)))
+
+(use-package org-agenda
+  :commands org-agenda
+  :config
+  (setq org-agenda-custom-commands
+        '(("x" "Unscheduled Tasks" tags-todo
+           "-SCHEDULED>=\"<today>\"-DEADLINE>=\"<today>\"" nil)
+          ("d" "Daily Tasks" agenda ""
+           ((org-agenda-span 1)))))
+  (setq org-agenda-skip-scheduled-if-done t)
+  (setq org-return-follows-link t)  ;; RET to follow link
+  (setq org-agenda-columns-add-appointments-to-effort-sum t)
+  (setq org-agenda-time-grid
+        '((daily today require-timed)
+          (0900 1200 1300 1800) "......" "----------------"))
+  (setq org-columns-default-format
+        "%68ITEM(Task) %6Effort(Effort){:} %6CLOCKSUM(Clock){:}")
+  (defadvice org-agenda-switch-to (after org-agenda-close)
+    "Close a org-agenda window when RET is hit on the window."
+    (progn (delete-other-windows)
+           (recenter-top-bottom)))
+  (ad-activate 'org-agenda-switch-to)
+  :bind
+  (:map org-agenda-mode-map
+        ("s" . org-agenda-schedule)
+        ("S" . org-save-all-org-buffers)))
+
+
+
+
+
+
 ;;;========================================
 ;;; キーバインド
 ;;; 追加機能とかのもあるのでさいご
@@ -162,7 +236,7 @@
  '(lsp-intelephense-files-max-size 10485760)
  '(package-selected-packages
    (quote
-    (magit cursor-chg typescript-mode twittering-mode mmm-mode vue-mode stylus-mode flymake lsp-mode mozc-popup mozc-im exec-path-from-shell markdown-preview-mode org-pomodoro geben-helm-projectile geben darcula-theme markdown-toc vmd-mode rainbow-delimiters highlight-indent-guides mozc js-doc add-node-modules-path eslint-fix prettier-js go-mode dockerfile-mode git-commit yaml-mode yasnippet-snippets helm-c-yasnippet yasnippet web-mode php-mode markdown-mode abyss-theme csv-mode json-mode neotree haskell-mode omnisharp ace-jump-mode undohist point-undo helm-helm-commands helm-pydoc helm-descbinds helm color-moccur company-statistics wgrep undo-tree pymacs popup nxml-mode js2-mode html5-schema)))
+    (use-package magit cursor-chg typescript-mode twittering-mode mmm-mode vue-mode stylus-mode flymake lsp-mode mozc-popup mozc-im exec-path-from-shell markdown-preview-mode org-pomodoro geben-helm-projectile geben darcula-theme markdown-toc vmd-mode rainbow-delimiters highlight-indent-guides mozc js-doc add-node-modules-path eslint-fix prettier-js go-mode dockerfile-mode git-commit yaml-mode yasnippet-snippets helm-c-yasnippet yasnippet web-mode php-mode markdown-mode abyss-theme csv-mode json-mode neotree haskell-mode omnisharp ace-jump-mode undohist point-undo helm-helm-commands helm-pydoc helm-descbinds helm color-moccur company-statistics wgrep undo-tree pymacs popup nxml-mode js2-mode html5-schema)))
  '(prolog-program-name
    (quote
     (((getenv "EPROLOG")
