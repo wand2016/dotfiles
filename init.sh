ab6ded9ccdbd6b3231d4ca317dbe909e1dde16b1
Daiki Horiyama <d19921207@gmail.com>
2019-11-22 19:21:52 +0900

    saying yes

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ npm i -g intelephense # php
 npm i -g vue-language-server # vue
 npm i -g typescript-language-server; npm i -g typescript #ts
 
-sudo add-apt-repository ppa:x4121/ripgrep
+sudo add-apt-repository -y ppa:x4121/ripgrep
 apt-get update -y && apt-get upgrade -y
 sudo apt-get install -y ripgrep
 
