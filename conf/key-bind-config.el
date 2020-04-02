23cc0becb5d3d3fcdf7a4a4be37a5578d378439c
Daiki Horiyama <d.horiyama@nextat.co.jp>
2020-04-02 10:11:20 +0900

    optionとcommandひっくり返すのやめる

--- conf/key-bind-config.el
+++ conf/key-bind-config.el
@@ -16,10 +16,10 @@
 ;;macos
 (when (eq system-type 'darwin)
   (progn
-    (setq mac-option-key-is-meta nil)
-    (setq mac-command-key-is-meta t)
-    (setq mac-command-modifier 'meta)
-    (setq mac-option-modifier nil)
+    ;; (setq mac-option-key-is-meta nil)
+    ;; (setq mac-command-key-is-meta t)
+    ;; (setq mac-command-modifier 'meta)
+    ;; (setq mac-option-modifier nil)
     (message "Mac OS X")))
 
 
