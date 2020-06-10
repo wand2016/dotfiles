ae1fff94325677a75d48cff8a912448999366bf2
Daiki Horiyama <d19921207@gmail.com>
2020-06-11 00:32:46 +0900

    sudo

--- init.sh
+++ init.sh
@@ -1,16 +1,16 @@
-npm i -g vmd
-npm i -g intelephense # php
-npm i -g vue-language-server # vue
-npm i -g typescript-language-server; npm i -g typescript #ts
+sudo npm i -g vmd
+sudo npm i -g intelephense # php
+sudo npm i -g vue-language-server # vue
+sudo npm i -g typescript-language-server; npm i -g typescript #ts
 
-npm i -g textlint \
+sudo npm i -g textlint \
          textlint-rule-preset-ja-spacing \
          textlint-rule-preset-ja-technical-writing \
          textlint-rule-spellcheck-tech-word
 
 
 sudo add-apt-repository -y ppa:x4121/ripgrep
-apt-get update -y && apt-get upgrade -y
+sudo apt-get update -y && apt-get upgrade -y
 sudo apt-get install -y ripgrep
 
 mkdir ~/.fonts
